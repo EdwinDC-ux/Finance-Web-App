40b54b62a75d14c7ca522179fef523a9b2a4fa4f
Edwin Díaz Caballero <edwin9602@gmail.com>
2026-09-05 21:52:25 -0600

    :sparkles: feat: Añadir un proxi para conectar vite con el contenedor php

---
+++ frontend/vite.config.ts
@@ -0,0 +1,15 @@
+// Archivo: frontend/vite.config.ts
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    host: true, // Necesario para Docker
+    proxy: {
+      // Todo lo que empiece con /api se va al contenedor de PHP
+      '/api': {
+        target: 'http://web:80', // 'web' es el nombre de tu servicio en docker-compose
+        changeOrigin: true,
+      }
+    }
+  }
+});
