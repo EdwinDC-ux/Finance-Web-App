72ba3738c09b0e5324da59e9d05fe4fddc124060
Edwin Díaz Caballero <edwin9602@gmail.com>
2026-09-08 09:52:43 -0600

    :sparkles: feat: Añadir loadStats al iniciar sesión o recargar la pagina.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import './style.css';
 import { showView } from './ui';
 import { initAuth } from './auth';
-import { initDashboard, loadAccounts, loadHistory } from './dashboard';
+import { initDashboard, loadAccounts, loadHistory, loadStats } from './dashboard';
 
 // 1. Inicializamos los eventos de los formularios
 initAuth();
@@ -10,4 +10,5 @@ initDashboard();
 // 2. Arrancamos la app
 showView('loading');
 loadAccounts();
+loadStats();
 loadHistory();
