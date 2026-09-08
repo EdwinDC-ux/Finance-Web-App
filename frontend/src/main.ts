ec9378a6ed3435e78e59cc066ee77981edf3e644
Edwin Díaz Caballero <edwin9602@gmail.com>
2026-09-08 09:57:56 -0600

    :sparkles: feat: Añadir loadCategories al main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import './style.css';
 import { showView } from './ui';
 import { initAuth } from './auth';
-import { initDashboard, loadAccounts, loadHistory, loadStats } from './dashboard';
+import { initDashboard, loadAccounts, loadHistory, loadStats, loadCategories } from './dashboard';
 
 // 1. Inicializamos los eventos de los formularios
 initAuth();
@@ -10,5 +10,6 @@ initDashboard();
 // 2. Arrancamos la app
 showView('loading');
 loadAccounts();
+loadCategories();
 loadStats();
 loadHistory();
